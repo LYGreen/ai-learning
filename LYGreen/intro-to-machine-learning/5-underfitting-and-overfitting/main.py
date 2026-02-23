a55159f41aff758a85b61fd7ea2c9ce71bb2d1a8
LYGreen <3098036603@qq.com>
2026-02-24 03:24:08 +0800

    Underfitting And Overfitting

---
+++ LYGreen/intro-to-machine-learning/5-underfitting-and-overfitting/main.py
@@ -0,0 +1,33 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+import pandas as pd
+    
+# Load data
+melbourne_file_path = './input/melb_data.csv' # 改了一下路径
+melbourne_data = pd.read_csv(melbourne_file_path) 
+# Filter rows with missing values
+melbourne_data = melbourne_data.dropna(axis=0)
+# Choose target and features
+y = melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
+                        'YearBuilt', 'Lattitude', 'Longtitude']
+X = melbourne_data[melbourne_features]
+
+from sklearn.model_selection import train_test_split
+
+train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
